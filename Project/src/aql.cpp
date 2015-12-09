c885c46b0c3d762a1937a54ca5e22aa483dc2002
camouflage <363419254@qq.com>
2015-12-09 10:33:16 +0800

    Add reading directory.

---
+++ Project/src/aql.cpp
@@ -0,0 +1,11 @@
+#include"tag.h"
+#include"token.cpp"
+#include"lexer.cpp"
+#include<iostream>
+#include<fstream>
+#include<cstring>
+using namespace std;
+
+void AQL(ifstream& aqlIfs, ifstream& documentIfs) {
+    
+}
--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,56 +1,77 @@
-#include"tag.h"
-#include"token.cpp"
-#include"lexer.cpp"
+#include"aql.cpp"
 #include<iostream>
 #include<fstream>
 #include<cstring>
+#include<dirent.h>
 
-#define LEN 100
+#define FILELEN 100
+#define NAMELEN 6
 using namespace std;
 
+// Return true if the extesion is .input
+bool isInput(char* fileName, int nameLen) {
+    char* extension = &fileName[nameLen];
+    return strcmp(extension, ".input") == 0;
+}
+
 int main(int argc, char* argv[]) {
-    char aqlPath[LEN];
-    snprintf(aqlPath, LEN, "../dataset/%s", argv[1]);
+    char aqlPath[FILELEN];
+    snprintf(aqlPath, FILELEN, "../dataset/%s", argv[1]);
     ifstream aqlIfs(aqlPath);
+    // AQL input
+    if ( !aqlIfs ) {
+        cerr << "Could not open aql file!" << endl;
+        return 2;
+    }
 
-    char documentPath[LEN];
-    int nameLen = strlen(argv[2]) - 6;
-    char* extension = &argv[2][nameLen];
-
+    char documentPath[FILELEN];
+    int nameLen = strlen(argv[2]) - NAMELEN;
+    
     // Single input file
-    if ( strcmp(extension, ".input") == 0 ) {
-        char lowerPath[LEN];
-        strncpy(lowerPath, argv[2], nameLen);
+    if (  isInput(argv[2], nameLen) ) {
+        // File name
+        char lowerNamePath[FILELEN];
+        strncpy(lowerNamePath, argv[2], nameLen);
         
-        // To lower case
-        for ( int i = 0; i < strlen(lowerPath); ++i ) {
-            lowerPath[i] = tolower(lowerPath[i]);
+        // To lower case: directory name
+        for ( int i = 0; i < strlen(lowerNamePath); ++i ) {
+            lowerNamePath[i] = tolower(lowerNamePath[i]);
         }
 
-        snprintf(documentPath, LEN, "../dataset/%s/%s", lowerPath, argv[2]);
+        snprintf(documentPath, FILELEN, "../dataset/%s/%s", lowerNamePath, argv[2]);
         ifstream documentIfs(documentPath);
         if ( !documentIfs ) {
             cerr << "Could not open document file!" << endl;
             return 1;
         } else {
-            /* tokenizer
-             * @params:
-                    documentIfs: ifstream
-               return: Vector of Word
-             */
-            // Tokenize(documentIfs);
-
+            // MAIN FUNCTION
+            AQL(aqlIfs, documentIfs);
         }
-    } else {
-        // TO DO: Specify directory
-    }
+    } else { // Given directory
+        snprintf(documentPath, FILELEN, "../dataset/%s", argv[2]);
+        // Ref: http://baike.baidu.com/link?url=sIJz87W5ckf74dcpsKIE1paczSx201EPFMND9J7aM2j-lzGs5l_6EdT9fd6mD-jVgrhJfAoHPY9CT9Je1xugF_
+        DIR* dp = opendir(documentPath);
 
-    // AQL
-    if ( !aqlIfs ) {
-        cerr << "Could not open aql file!" << endl;
-        return 2;
-    } else {
-        // lexer
-        lex(aqlIfs);
+        if ( dp == NULL ) {
+            cerr << "Could not find the directory!" << endl;
+            return 3;
+        } else {
+            struct dirent* dirp = readdir(dp);
+            while ( dirp != NULL ) {
+                // Get input file name
+                if ( isInput(dirp->d_name, strlen(dirp->d_name) - NAMELEN) ) {
+                    snprintf(documentPath, FILELEN, "../dataset/%s/%s", argv[2], dirp->d_name);
+                    ifstream documentIfs(documentPath);
+                    if ( !documentIfs ) {
+                        cerr << "Could not open document file!" << endl;
+                        return 1;
+                    } else {
+                        // MAIN FUNCTION
+                        AQL(aqlIfs, documentIfs);
+                    }
+                }
+                dirp = readdir(dp);
+            }
+        }
     }
 }
